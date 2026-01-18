0fc57be891f64287a69927e67960ab7090dc710a
OrbinaryNobody <makarworkmail@gmail.com>
2026-01-18 14:30:19 +0500

    ф

---
+++ backend/src/main/java/com/adapstory/course/CourseTrackApplication.java
@@ -0,0 +1,12 @@
+package com.adapstory.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseTrackApplication.class, args);
+    }
+}
